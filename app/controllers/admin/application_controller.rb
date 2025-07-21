6c77f51947984fdb453a320236188a31bcaa7aa6
hayashida <kenyalintian@gmail.com>
2025-07-21 20:28:22 +0900

    従業員側ログイン機能の実装

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,12 +1,10 @@
-class ApplicationController < ActionController::Base
-  def after_sign_in_path_for(resource)
-    case resource
-    when AdminUser
-      admin_dashboard_path # 管理者ログイン後に飛ばすパス
-    when User
-      user_dashboard_path # 従業員ログイン後に飛ばすパス
-    else
-      root_path
-    end
+def after_sign_in_path_for(resource)
+  case resource
+  when AdminUser
+    admin_dashboard_path # 管理者は管理用ダッシュボードへ
+  when User
+    posts_path           # 一般ユーザーは /posts に飛ばす
+  else
+    root_path
   end
 end
